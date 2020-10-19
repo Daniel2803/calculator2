f86210c8e61a535f6d16b838fdd51ddaebe5be92
Daniel2803 <49502893+Daniel2803@users.noreply.github.com>
2020-10-20 08:00:19 +1300

    Add files via upload

---
+++ calculator.js
@@ -0,0 +1,10 @@
+
+
+function calcNumbers(result){
+        form.displayResult.value=form.displayResult.value+result;
+document.getElementById("txtMain").value += result;
+
+    }
+    function clean(){
+      document.form.displayResult.value ="";
+    }
